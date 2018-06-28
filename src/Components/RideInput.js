d57c1ec94beb1d887f85f48da541a7e8d1278991
Li YungHao <liyunghao@gmail.com>
2018-06-29 03:03:53 +0800

    new

--- src/Components/RideInput.js
+++ src/Components/RideInput.js
@@ -107,13 +107,13 @@ class RideInput extends React.Component{
                   <div className="input-group-prepend">
                     <span className="input-group-text" id="inputGroup-sizing-sm">開始時間</span>
                   </div>
-                  <input type="text" className="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm" value = {this.state._data.startTime} onChange={ (e) => this.updateStartTime(e.target.value)} />
+                  <input type="datetime-local" className="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm" value = {this.state._data.startTime} onChange={ (e) => this.updateStartTime(e.target.value)} />
                 </div>
                 <div className="input-group input-group-sm mb-3">
                   <div className="input-group-prepend">
                     <span className="input-group-text" id="inputGroup-sizing-sm">結束時間</span>
                   </div>
-                  <input type="text" className="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm" value = {this.state._data.endTime} onChange={ (e) => this.updateEndTime(e.target.value)} />
+                  <input type="datetime-local" className="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm" value = {this.state._data.endTime} onChange={ (e) => this.updateEndTime(e.target.value)} />
                 </div>
                 <div className="input-group input-group-sm mb-3">
                   <div className="input-group-prepend">
