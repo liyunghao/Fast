cbb1881cf45f139b72ad273eddfb754f3e38bf7f
Li YungHao <liyunghao@gmail.com>
2018-06-29 13:34:34 +0800

    Setting css

--- src/Pages/Setting.js
+++ src/Pages/Setting.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import '../Css/Setting.css'
 class Setting extends React.Component {
 
 
@@ -11,11 +11,10 @@ class Setting extends React.Component {
     this.state = {
       rando :'',
       text: '',
-      lift: []
     }
     this.updateDropdownTitle = this.updateDropdownTitle.bind(this)
-     this.updateText = this.updateText.bind(this)
-      this.setlift = this.setlift.bind(this)
+    this.updateText = this.updateText.bind(this)
+  
   }
 	updateDropdownTitle(t){
 		this.setState({ rando: t })
@@ -24,50 +23,39 @@ class Setting extends React.Component {
 			this.setState({text: t})
 
 	}
-	setlift(){
-		let tmp = this.state.lift
-		tmp.push(this.state.text)
-		this.setState({lift:tmp})
-	}
-	
-
 			
 render(){
 		return(
-				<div >
-	{console.log(this.state.lift)}
-    <div className='container'>我喜歡收到什麼禮物</div>
-    <div className='container'>
-    <div className='row'>
-    <div className='col-9'>
-    <input value={this.state.text} className='mx-auto  col-9  in' type='text' onChange={ (e) => this.updateText(e.target.value) }></input>
-    <button className=' btn btn-success   col-2'  onClick={this.setlift} >新增</button>
+		  <div>
+        <span className = 'text'>我喜歡收到什麼禮物</span>
+          <div className='container'>
+            <div className='row'>
+              <div className='col-12'>
+                <textarea value={this.state.text} className='mx-auto  col-9  textbox' type='text' onChange={ (e) => this.updateText(e.target.value) }></textarea>
+                {/* <button className=' btn btn-success  ' onClick={this.setlift} >更新</button> */}
+              </div>
+            </div>
+          </div>
+          <div className='container'>
+          {/* {this.state.lift} */}
+          </div>
+    
+        <div className="recordbox">
+          <ul className="list-group">
+            <li className='list-group-item'>現有點數:197點</li>
+            <br/>
+            <li className='list-group-item'>我的評分:4.9/5</li>
+            <br/>
+            <li className='list-group-item'>交通記錄: 2018/6/25 和甜不辣、雞蛋豆腐、湯圓去  巨城</li>
+            <br/>
+            <li className='list-group-item'>2018/5/30   和鍋燒面去清頁</li>
+            <br/>
+            <li className='list-group-item'>2018/5/24   和張於燒去  揪團去吃冰</li>
+            <br/>
+            <li className='list-group-item'>2018/4/02  和鹽酥雞當了一段walk buddy</li>
+          </ul> 
+        </div>
     </div>
-    </div>
-    </div>
-    <div className='container'>
-    {this.state.lift}
-	</div>
-	
-    <div Name="recordbox">
-    <ul className="myrecordul">
-      <li className='myrecordli1'>現有點數:197點</li>
-      <br/>
-      <li className='myrecordli2'>我的評分:4.9/5</li>
-      <br/>
-      <li className='myrecordli3'>交通記錄: 2018/6/25 和甜不辣、雞蛋豆腐、湯圓去  巨城</li>
-      <br/>
-      <div className="floattoright">
-
-        <li className='myrecordli4'>2018/5/30   和鍋燒面去清頁</li>
-        <br/>
-        <li className='myrecordli5'>2018/5/24   和張於燒去  揪團去吃冰</li>
-        <br/>
-        <li class='myrecordli6'>2018/4/02  和鹽酥雞當了一段walk buddy</li>
-      </div>
-    </ul> 
-    </div>
-</div>
 		)
 	}
 }
